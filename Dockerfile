27939a56159817214bbcbde0a5da6be68be5e049
Christian Rivera <christianrivera@christians-mbp.mynetworksettings.com>
2022-12-13 18:03:43 -0500

    Adding dockerfile

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.10
+WORKDIR /app
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+COPY . .
+CMD ["gunicorn", "--bind", "0.0.0.0:80", "app:create_app()"]
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import pickle
+import csv
 
 app = Flask(__name__)
 
@@ -8,3 +9,10 @@ def get_prediction():
     request_data = request.get_json()
     print(request_data)
     return {'prediction': 100}
+
+def get_win_percentage(team):
+    games = 0
+    wins = 0
+
+    # with open('preprocessed_results.csv') as csvfile:
+
